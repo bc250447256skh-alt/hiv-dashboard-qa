82d0223bdd5c93dc3f4f88be233a7922babd1858
bc250447256skh-alt <bc250447256skh@vu.edu.pk>
2026-02-16 14:12:30 -0500

    Run Cypress against AWS deployed environment

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000"
+    baseUrl: 'http://hiv-dashboard-env.eba-yewzkg2q.us-west-2.elasticbeanstalk.com'
   }
 });
